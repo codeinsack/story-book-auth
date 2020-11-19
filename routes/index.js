995140ee5a4ac6ba1a9faf513d98511f7dbb2d8a
codeinsack <codeinsack@gmail.com>
2020-11-19 18:13:25 +0300

    filter stories by user

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,25 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, insureGuest } = require('../middleware/auth')
 
+const Story = require('../models/Story')
+
 router.get('/',  insureGuest, (req, res) => {
   res.render('login', {
     layout: 'login'
   })
 })
 
-router.get('/dashboard', ensureAuth, (req, res) => {
-  res.render('dashboard', {
-    name: req.user.firstName,
-  })
+router.get('/dashboard', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id })
+      .lean()
+    res.render('dashboard', {
+      name: req.user.firstName,
+      stories,
+    })
+  } catch (error) {
+    console.error(error)
+  }
 })
 
 module.exports = router
